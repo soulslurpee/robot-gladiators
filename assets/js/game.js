0a7bb4cc4d9132a255ddf335d185aa7a8802001a
soulslurpee <charles.d.thompson87@gmail.com>
2022-02-22 11:13:58 -0500

    finished for loop with enemy names

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,18 +7,6 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-
-for(var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-  }
-
-
 var fight = function(enemyName) {
 
     window.alert("Welcome to Robot Gladiators!");
@@ -67,4 +55,6 @@ var fight = function(enemyName) {
     }
 }
 
-// fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
